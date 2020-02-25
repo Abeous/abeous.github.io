c754c2fe9455e0c50f581c1ec33a268c6c761556
Abeous <kingmrkrab1@gmail.com>
2020-02-25 06:13:05 -0500

    WIP mentions viewer 0.2

--- mentions/js/mentions.js
+++ mentions/js/mentions.js
@@ -1,4 +1,3 @@
-const emotes_json = require('../emotes.json')
 
 function getUrlVars() {
   var vars = {};
@@ -8,22 +7,65 @@ function getUrlVars() {
   return vars;
 }
 
-// $.get("https://cors-anywhere.herokuapp.com/" + args.url, function(data) {
-//   document.getElementById("display").innerHTML = data
-// })
-
+const GENERIFY_OPTIONS = {
+  'mirror': 'mirror',
+  'flip': 'flip',
+  'rain': 'weather rain anim-fix',
+  'snow': 'weather snow anim-fix',
+  'rustle': 'generify-rustle',
+  'worth': 'generify-worth',
+  // 'dank': 'generify-dank',
+  'hyper': 'generify-hyper generify-rustle',
+  'love': 'generify-love',
+  'spin': 'generify-spin',
+  'wide' : 'wide',
+  'virus' : 'virus',
+  'banned' : 'banned',
+  // 'frozen': 'frozen',
+  'lag' : 'lag',
+};
 
 function formatEmote(data) {
   emote = data.split(":")[0]
   modifier = data.split(":")[1]
-
-  return `<span class="generify-container "><span title=" ${emote}" class="chat-emote chat-emote-${emote} chat-emote-${emote}-animate-forever>Wowee</span></span>`
+  
+  return `<span class="generify-container generify-emote-${emote}"><span title=" ${emote}" class="chat-emote chat-emote-${emote} chat-emote-${emote}-animate-forever></span></span>`
 }
 
-function formatMessage() {
-  // document.getElementById("display").innerText = formatEmote("Wowee")
-  document.getElementById("display").innerHTML = "wowee"
+function formatMessage(line, message) {
+  username = line.split('|')[1].split(':')[0]
+  line_message = line.substring(line.indexOf(username) + username.length)
+
+  return `<div class="msg-chat msg-user msg-highlight"><a class="user ">${username}</a><span class="ctrl">: </span> <span class="text">${line_message}</span></div>`
+}
+
+function setDisplay() {
+  args = getUrlVars()
+  $.get("https://cors-anywhere.herokuapp.com/" + args.url, function(data) {
+    lines = data.split('\r\n')
+    $.getJSON("./emotes.json", function(json) {
+      emotesArray = Object.values(json.default)
+      regex = new RegExp(`(^|\\s)(${emotesArray.join('|')})(:(${Object.keys(GENERIFY_OPTIONS).join('|')}))?(?=$|\\s)`, 'gm')
+      var formatted_lines = []
+      for (line in lines) {
+        for (emote in emotesArray) {
+          (lines[line].includes(emotesArray[emote]))
+          ? formatted_lines[line] = lines[line].replace(regex, formatEmote(emotesArray[emote]))
+          : formatted_lines[line] = lines[line]
+        }
+      }
+
+      for (line in formatted_lines) {
+        var newLine = formatMessage(lines[line])
+        console.error(formatted_lines[line])
+        document.getElementById("chat_display").insertAdjacentHTML('beforeend', newLine)
+      }
+    })
+  })
 }
 
 
-document.getElementById("display").onloadend = formatMessage()
+
+window.onload = function() {
+  this.setDisplay()
+}
