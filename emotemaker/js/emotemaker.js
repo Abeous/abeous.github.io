84ad5a886d78efc45663b9bc570148fc44972500
Abeous <abeouss@gmail.com>
2020-03-20 07:51:06 -0400

    UI & custom height & dragndrop update

--- emotemaker/js/emotemaker.js
+++ emotemaker/js/emotemaker.js
@@ -1,33 +1,38 @@
-function GCD(a,b) {
-  return (b == 0) ? a : GCD (b, a%b);
-}
-
-
-
-function setImage(img, img_width, img_height) {
-  var emoteElements = $('.chat-emote.chat-emote-MyNewEmote')
-
-  new_width = (img_width * 32) / img_height
-  new_height = 32
-  
-  for (element in emoteElements) {
-    emoteElements[element].style.backgroundImage = `url("${img}")`
-    emoteElements[element].style.width = new_width + 'px'
-    emoteElements[element].style.height = new_height + 'px'
-    emoteElements[element].style.backgroundSize = `${new_width}px ${new_height}px`
-  }
-}
-
-
-
-
-function changeEmoteImage() {
-  var URL = document.getElementById('emote_input').value
-
+function setEmotes(url) {
   var img = new Image();
-  img.src = URL;
-  img.onload = function() {
-    setImage(this.src, this.width, this.height)
+  img.src = url
+  img.onload = function () {
+    var emoteElements = $('.chat-emote.chat-emote-MyNewEmote')
+    var custom_height_value = $("#custom-height-input").val()
+
+    var new_height;
+    if (custom_height_value !== ''){
+      new_height = custom_height_value
+    } else {
+      new_height = 32
+    }
+    
+    new_width = (this.width * new_height) / this.height
+
+    for (element in emoteElements) {
+      emoteElements[element].style.backgroundImage = `url("${url}")`
+      emoteElements[element].style.width = new_width + 'px'
+      emoteElements[element].style.height = new_height + 'px'
+      emoteElements[element].style.backgroundSize = `${new_width}px ${new_height}px`
+    }
   }
 }
 
+
+function drop(event) {
+  event.preventDefault()
+  var data = event.dataTransfer.getData("text")
+  setEmotes(data)
+}
+
+
+window.onload = function () {
+  $('#emote-submit-button').click(() => {
+    this.setEmotes($("#emote_input").val())
+  })
+}
